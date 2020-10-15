1e4c0165e0b686b4ede1a7b66a03433ada8154c7
Logan Hemphill <logan.hemphill86@gmail.com>
2020-10-15 13:44:45 -0400

    Updating portfolio

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const projOne = document.querySelector('.proj1').addEventListener('click', function() {
     window.location.href = "https://createyourjourney.herokuapp.com/";
 })
+
+const projTwo = document.querySelector('.proj2').addEventListener('click', function() {
+    window.location.href = "https://quiz-app-project.herokuapp.com/";
+})
+
+const projThree = document.querySelector('.proj3').addEventListener('click', function() {
+    window.location.href = "https://retro-feed.herokuapp.com/";
+})
